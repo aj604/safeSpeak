86f131c642956b77b87ef3dacb695df524d7dc13
Nafeh Shoaib <nafehshoaib14@gmail.com>
2018-05-27 09:16:42 -0400

    Final Node Submission

---
+++ botTestFile.py
@@ -0,0 +1,6 @@
+import sys
+
+data = sys.argv[1];
+
+print("Data was received: " + data);
+sys.stdout.flush()
--- discordbot/node/bot.js
+++ discordbot/node/bot.js
@@ -1,16 +1,52 @@
 const botSettings = require("./botsettings.json");
+
+// Discord bot
 const Discord = require("discord.js");
 
+// Writing to Files
 const fs = require("fs");
+var FileWriter = require('wav').FileWriter;
 
+var PythonShell = require('python-shell');
+
+// Calling Python script
+var spawn = require("child_process").spawn;
+
+// Discord bot instance
 const bot = new Discord.Client({
 
 });
 
+// general Text Channel
+var textChannel = bot.channels.find("name", "general");
+
+var currentFileName = "";
+
+// save audio as wav file
 function generateOutputFile(channel, member) {
     // use IDs instead of username cause some people have stupid emojis in their name
-    const fileName = `./recordings/${channel.id}-${member.id}-${Date.now()}.pcm`;
-    return fs.createWriteStream(fileName);
+    const fileName = `./recordings/${channel.id}-${member.id}-${Date.now()}.wav`;
+    currentFileName = fileName;
+    var outputFileStream = new FileWriter(fileName);
+    return outputFileStream;
+    //return fs.createWriteStream(fileName);
+}
+
+// run python script
+async function runPythonScript(path) {
+    var options = {
+        args:
+        [
+            path// audioFilePath
+        ]
+    }
+
+    PythonShell.run('../../botTestFile.py', options, function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(data.toString());
+    });
 }
 
 bot.on('message', async (message) => {
@@ -19,8 +55,8 @@ bot.on('message', async (message) => {
     let command = messageArr[1];
     let channelName = messageArr[2];
     
-    if ((pre === bot.user.username) && (command === "start")) {
-        const voiceChannel = message.guild.channels.find("name", channelName.join(" "));
+    //if ((pre === bot.user.username) && (command === "start")) {
+        /* const voiceChannel = message.guild.channels.find("name", channelName.join(" "));
 
         if (!voiceChannel || voiceChannel.type !== 'voice') {
             return message.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
@@ -45,14 +81,17 @@ bot.on('message', async (message) => {
                     });
                 }
             });
-        }).catch(console.log);
-    }
-    if ((pre === bot.user.username) && (command === "leave")) {
+        }).catch(console.log); */
+    //}
+    /* //if ((pre === bot.user.username) && (command === "leave")) {
         let voiceChannel = message.guild.channels.find("name", channelName.join(" "));
         voiceChannel.leave();
-    }
-})
+    } */
+});
 
+
+
+// bot start up code
 bot.on("ready", async () => {
     console.log("bot is ready, user is: " + bot.user.username);
     try {
@@ -61,6 +100,37 @@ bot.on("ready", async () => {
     } catch(error) {
         console.log(error.stack);
     }
+
+    const channelName = "General";
+    const voiceChannel = bot.channels.find("name", channelName);
+
+    if (!voiceChannel || voiceChannel.type !== 'voice') {
+        //return message.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
+    }
+    voiceChannel.join().then(bot => {
+        //message.reply('ready!');
+        const receiver = bot.createReceiver();
+
+        bot.on('speaking', (user, speaking) => {
+            if (speaking) {
+                //message.channel.sendMessage(`I'm listening to ${user}`);
+
+                const audioStream = receiver.createPCMStream(user);
+
+                const outputStream = generateOutputFile(voiceChannel, user);
+
+                audioStream.pipe(outputStream);
+                outputStream.on("data", console.log);
+
+                audioStream.on('end', () => {
+                    
+                    runPythonScript(currentFileName);
+                    //textChannel.sendMessage(result);
+                    //message.channel.sendMessage(`I'm no longer listening to ${user}`);
+                });
+            }
+        });
+    }).catch(console.log);
 });
 
 bot.login(botSettings.token);
